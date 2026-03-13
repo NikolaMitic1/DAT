f9e88eaa8fca5a6ae3a5f568e44668f350d22fc3
Nikola Mitic <nmitic304@gmail.com>
2026-03-13 14:13:08 -0400

    Dodavanje Enum-a

---
+++ backend/src/main/java/com/example/backend/enums/Equipment.java
@@ -0,0 +1,9 @@
+package com.example.backend.enums;
+
+public enum Equipment {
+    DRY_VAN,
+    REEFER,
+    FLATBED,
+    STEP_DECK,
+    POWER_ONLY
+}
---
+++ backend/src/main/java/com/example/backend/enums/Status.java
@@ -0,0 +1,9 @@
+package com.example.backend.enums;
+
+public enum Status {
+    POSTED,
+    BOOKED,
+    IN_TRANSPORT,
+    DELIVERED,
+    CANCELLED
+}
