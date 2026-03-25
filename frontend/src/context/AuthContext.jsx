e44bea5235008d6c07056f7079a21befc80e4a7d
Nikola Mitic <nmitic304@gmail.com>
2026-03-25 14:04:45 -0400

    Greska!

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const loginUser = (data) => {
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("role", data.role);
+    setUser({ token: data.token, role: data.role });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
